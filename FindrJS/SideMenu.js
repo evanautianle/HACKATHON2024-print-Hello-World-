393cf3a2db534e248760f3e3d4dc672ccde27592
Evan Au <evan.au206@gmail.com>
2024-08-03 13:22:46 +1200

    bug fix

--- FindrJS/SideMenu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    const menu = document.getElementById('menu');
-
-    hamburgerMenu.addEventListener('click', () => {
-        hamburgerMenu.classList.toggle('open');
-        menu.classList.toggle('show');
-    });
-});
